bd671c1837f5f4015e23f8abd8cc00843bdc0ddb
Vishal Prabhu <vishal.prabhu@gmail.com>
2022-01-02 23:20:09 -0800

    Goodbye i3

--- i3/.config/i3/i3lock-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ICON=~/.config/i3/icon.png
-TMPBG=/tmp/screenshot-for-lock.png
-import -window root $TMPBG
-convert $TMPBG -scale 10% -scale 1000% $TMPBG
-convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
-i3lock -u -i $TMPBG
-rm $TMPBG
-
